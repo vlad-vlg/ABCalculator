9231f35accdea27c19cff99834006a360fe8b009
Vladimir <vlad_e@rambler.ru>
2023-09-03 21:30:42 +0300

    Добавление файла программы калькулятора

---
+++ ABCalculator.py
@@ -0,0 +1,11 @@
+# А/В калькулятор
+
+import tkinter as tk
+
+# Создание главного окна
+root = tk.Tk()
+root.geometry('280x300+300+200')
+root.title('А/В калькулятор')
+
+# Запуск цикла mainloop
+root.mainloop()
