b37dc8194b9323cfc1c5782202160ad01e3742bd
Vladimir <vlad_e@rambler.ru>
2023-09-03 21:43:15 +0300

    Добавление метки заголовка

--- ABCalculator.py
+++ ABCalculator.py
@@ -7,5 +7,9 @@ root = tk.Tk()
 root.geometry('280x300+300+200')
 root.title('А/В калькулятор')
 
+# Добавление метки заголовка
+lblTitle = tk.Label(root, text='А/В калькулятор', font=('Helvetica', 16, 'bold'), fg='#0000cc')
+lblTitle.place(x=55, y=25)
+
 # Запуск цикла mainloop
 root.mainloop()
